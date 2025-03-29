c680af03ea1cb306d7e70de4c40ddee4ef95db20
Dani81mz <danielnessfotografia@gmail.com>
2025-03-29 10:51:57 +0100

    commit 1

---
+++ main.js
@@ -0,0 +1,74 @@
+const menuBtn = document.getElementById("menu-btn");
+const navLinks = document.getElementById("nav-links");
+const menuBtnIcon = menuBtn.querySelector("i");
+
+menuBtn.addEventListener("click", (e) =>{
+    navLinks.classList.toggle("open");
+
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute(
+        "class",
+        isOpen ? "ri-close-line" : "ri-menu-3-line"
+    );
+});
+
+navLinks.addEventListener("click", (e) => {
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class", "ri-menu-3-line");
+});
+
+const scrollRevealOptions = {
+    distance: "50px",
+    origin: "bottom",
+    duration: 1000,
+};
+
+ScrollReveal().reveal(".header_content h1", {
+    ...scrollRevealOptions,
+});
+ScrollReveal().reveal(".header_content h2", {
+    ...scrollRevealOptions,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".header_btn", {
+    ...scrollRevealOptions,
+    delay: 500,
+});
+
+ScrollReveal().reveal(".service_card", {
+    ...scrollRevealOptions,
+    interval: 500,
+});
+ScrollReveal().reveal(".price_card", {
+    ...scrollRevealOptions,
+    interval: 500,
+});
+
+
+
+const swiper = new Swiper(".swiper", {
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
+
+const filterButtons = document.querySelectorAll(".filter_buttons button");
+const filterableCards = document.querySelectorAll(".filterable_cards .card");
+
+const filterCards = e => {
+    document.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+
+    filterableCards.forEach(card => {
+        card.classList.add("hide");
+
+        if(card.dataset.name === e.target.dataset.name || e.target.dataset.name === "all"){
+            card.classList.remove("hide");
+        }
+    });
+};
+
+filterButtons.forEach(button => button.addEventListener("click", filterCards));
+
